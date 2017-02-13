fb5093cbcbd63dd104cc9162236636b9a47edfa3
Mark Calder <mark.calder@truedigital.co.uk>
2017-02-13 15:35:51 +0000

    update package.json

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("true_power_wahey");
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8025);
 
 // Put a friendly message on the terminal
