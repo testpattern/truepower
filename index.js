47b6332540021fde2adcaccf30cc43815df8314b
Mark Calder <mark.calder@truedigital.co.uk>
2017-02-14 09:29:25 +0000

    try fb code

--- index.js
+++ index.js
@@ -24,18 +24,21 @@ app.get('/', function (req, res) {
 })
 
 // for facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	} else {
-		res.send('Error, wrong token')
-	}
-})
+// app.get('/webhook/', function (req, res) {
+// 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+// 		res.send(req.query['hub.challenge'])
+// 	} else {
+// 		res.send('Error, wrong token')
+// 	}
+// })
 
 // to post data
 app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
+    console.log('webhook!')
+    console.log(messaging_events)
 	for (let i = 0; i < messaging_events.length; i++) {
+        console.log(i)
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
