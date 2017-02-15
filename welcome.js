c13e4d47e13c1c17dc9b930ad59fd5de6023ee04
Mark Calder <mark.calder@truedigital.co.uk>
2017-02-15 11:01:37 +0000

    responders

--- welcome.js
+++ welcome.js
@@ -1,5 +1,10 @@
 'use strict'
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
 class Welcome {
     respond(sender){
         let messageData = {        
