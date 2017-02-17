a5579d5e3df86161eec7c6920877a85af844730a
Mark Calder <mark.calder@truedigital.co.uk>
2017-02-17 16:13:37 +0000

    cleanup

--- responder.js
+++ responder.js
@@ -59,68 +59,6 @@ class Responder {
             }
         })
     }
-
-    nextResponse(sender, text, token) {
-        // this will construct the response based on the postback text
-        // something like an array of functions and each one contructs its own message
-        var section = text.split('.')[0];
-        var name = text.split('.')[1];
-        let message = this.respond(section, name);
-        
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token:token },
-            method: 'POST',
-            json: {
-                recipient: {id:sender},
-                message: message,
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-        })
-    }
-
-    generic(sender,token) {
-        let messageData = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Hi. What can I help you with today – is your question about a (recent) bill?",
-                        "buttons": [{
-                            "type": "postback",
-                            "title": "Yes",
-                            "payload": "isBill-YES",
-                        },{
-                            "type": "postback",
-                            "title": "No",
-                            "payload": "isBill-NO",
-                        }],
-                    }]
-                }
-            }
-        }
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token:token},
-            method: 'POST',
-            json: {
-                recipient: {id:sender},
-                message: messageData,
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-        })
-    }
 }
 
 module.exports = Responder;
